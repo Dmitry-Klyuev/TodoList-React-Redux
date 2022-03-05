08cc6b2056e1683a422b890a6f2ce5f665f049b6
Dmity-kl <i6169696@gmail.com>
2022-03-05 22:37:36 +0300

    SetState

--- src/components/todoListItem/todoListItem.js
+++ src/components/todoListItem/todoListItem.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import './todoListItem.css'
 
 export class TodoListItem extends React.Component {
 
+   state= {
+       done: false
+   }
+
     onLabelClickHandler = () => {
-        console.log(`Done: ${this.props.label} `);
+        this.setState( {
+            done: !this.state.done
+        })
     };
 
     render() {
         const {label, important = false} = this.props;
+        const {done} = this.state
+
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal',
         };
         return (
-            <span className="todo-list-item">
+            <span className={this.state.done ? 'todo-list-item done' : 'todo-list-item'}>
       <span
           className="todo-list-item-label"
           style={style}
